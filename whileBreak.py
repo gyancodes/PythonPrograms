a9d59a5ddc8376c8089af059df83ad283dfa3ad6
ABHAY SHEKHAR YADAV <your-email-address>
2019-02-25 23:35:41 +0530

    while and break statement

---
+++ whileBreak.py
@@ -0,0 +1,11 @@
+while True:
+    print('who are you')
+    name=input()
+    if name!='abhay':
+        print('wrong pwd')
+    print('Hey abhay ! whats the passowrd')
+    password=input()
+    if password == 'ekaasman55':
+            break
+print('Acess granted')
+

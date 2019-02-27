f9e69c4d2f2c08e44787dbe88e32f83585a8f9d2
ABHAY SHEKHAR YADAV <your-email-address>
2019-02-27 11:23:42 +0530

    while and break statement

--- whileBreak.py
+++ whileBreak.py
@@ -2,10 +2,11 @@ while True:
     print('who are you')
     name=input()
     if name!='abhay':
-        print('wrong pwd')
+        continue
     print('Hey abhay ! whats the passowrd')
+    print('enter your password')
     password=input()
-    if password == 'ekaasman55':
-            break
+    if password=='55':
+     break
 print('Acess granted')
 

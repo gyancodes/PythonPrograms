0b40dbbf028b857e8c44a7e68e9700733280a285
ABHAY SHEKHAR YADAV <your-email-address>
2019-02-27 12:06:25 +0530

    using import

---
+++ import.py
@@ -0,0 +1,4 @@
+import random
+#printing 100 random numbers between 1 and 100
+for i in range(100):
+    print(random.randint(1,100))
--- loopAndRange.py
+++ loopAndRange.py
@@ -8,3 +8,7 @@ total=0
 for num in range(10):
     total=total+num
 print(total)
+#for loop with start and stop condition*/
+
+for i in range(1,10,1):
+    print(i)

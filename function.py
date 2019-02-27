62c22230c755272a655dd17e1db452c1a6c29da5
ABHAY SHEKHAR YADAV <your-email-address>
2019-02-27 13:53:57 +0530

    defining functions

---
+++ function.py
@@ -0,0 +1,12 @@
+#non parametric function
+def hello():
+    for i in range(5):
+        print('Hello world')
+
+ #parameterised function
+def Hello(name):
+    print('Hi '+ name+'!!')
+
+
+hello()
+Hello('Abhay')

29c3ae5a8a433627b085b2f4470cb20ab1f07991
ABHAY SHEKHAR YADAV <your-email-address>
2019-02-28 19:02:38 +0530

    using end and sep argument of print()

---
+++ argumentsOfPrint.py
@@ -0,0 +1,10 @@
+print('hello','Abhay')
+
+#using end argument
+print('Hello',end=' ')      #empty string is passed as the argument in print function
+print('Boss')
+
+print('Good','Morning','!!')
+
+#using sep argument
+print('Abhay','Tanuj','Leon',sep=',')   #sep argument to separate the string by ,

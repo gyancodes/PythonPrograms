68c35da25f84399bbff559290637a13b9d79560a
ABHAY SHEKHAR YADAV <your-email-address>
2019-03-03 21:56:44 +0530

    using getPass module

--- getPass.py
+++ getPass.py
@@ -1,8 +1,9 @@
 #using getpass module to enter password without making pwd visible
 import getpass
 
+# enter oxford as a password just for trial
 password=getpass.getpass(prompt='Enter the password:')
-if password==oxford:            #enter oxford as a password just for trial
+if password=='oxford':
     print('Welcome to the world of python programming')
 else:
     print('Incorrect password ...sorry you cannot read our book')

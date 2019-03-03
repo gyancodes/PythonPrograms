e7f7debb28b913e60a36dc0c6d092b4f48cd34c1
ABHAY SHEKHAR YADAV <your-email-address>
2019-03-03 11:51:28 +0530

    guessingSecretNumberGame

---
+++ secretNumberGuessing.py
@@ -0,0 +1,16 @@
+import random
+secretNumber=random.randint(1,20)
+print('I am guessing a number between 1 to 20')
+for guessTaken in range(1,7):
+    print('Take your guess')
+    guess=int(input())
+    if guess<secretNumber:
+        print('Your guess is too low')
+    elif guess>secretNumber:
+        print('Your guess is too high')
+    else:
+        break
+if guess==secretNumber:
+    print('Good job!You guessed correct number in '+ str(guessTaken) + 'guesses!')
+else:
+    print('Niope! your guess was not corect ')

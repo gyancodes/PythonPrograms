1c1fafe25e729a325d1b63994b3223018065d2b1
ABHAY SHEKHAR YADAV <your-email-address>
2019-03-05 22:25:57 +0530

    inAndNotinOperator

---
+++ inAndNotInOperator.py
@@ -0,0 +1,9 @@
+sent='My name is abhay shekhar yadav'
+print (sent)
+name='abhay'
+cast='mishra'
+if name in sent:
+    print ('abhay foud in the sentence')
+if cast not in sent:
+    print ('mishra not found in the sentence')
+

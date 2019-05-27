46e5c67f0b7d9d5587ccd058ba7e7c50cdb23e86
ABHAY SHEKHAR YADAV <your-email-address>
2019-05-28 02:26:00 +0530

    moving curser to different positions on th escreen

--- mouseNow.py
+++ mouseNow.py
@@ -8,7 +8,11 @@ try:
         positionstr='X:' +  str(x).rjust(4)+ 'Y:' +str(y).rjust(4)
         print(positionstr, end='')
         print('\b'*len(positionstr),end='',flush=True)
-        pyautogui.click(x,y)
+        pyautogui.click(15,37)
+        time.sleep(1)
+        pyautogui.click(26,71)
+
+
 except KeyboardInterrupt:
     print('\n Done')
 

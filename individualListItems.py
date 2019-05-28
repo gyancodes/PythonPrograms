4ccb73f16ccce2354b563ffd97e314749355b12c
ABHAY SHEKHAR YADAV <your-email-address>
2019-05-29 00:16:27 +0530

    slicing the list and printing items

--- individualListItems.py
+++ individualListItems.py
@@ -1,5 +1,14 @@
 #printing the individual list items
 
-name=['abhay','tanuj','leon','devansh']
+name=['abhay','tanuj','leon','devansh']*3
 for i in range(len(name)):
     print(name[i])
+
+
+
+#slicing the list
+title=['yadav','patel','gupta','singh','thakur','rajput','jindal']
+print(title[1:4])
+#printing the sliced list items
+for j in range(len(title[1:4])):
+    print(title[j])

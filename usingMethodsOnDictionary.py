0eecdf70e39a2acd4f136efa56688134abc9cf99
ABHAY SHEKHAR YADAV <your-email-address>
2019-05-29 22:21:47 +0530

    counting each character in the string

--- usingMethodsOnDictionary.py
+++ usingMethodsOnDictionary.py
@@ -1,10 +1,24 @@
-spam={'color':'red','age':42}
-for i in spam.values():         #value() method
+spam={'color':'red','age':'42'}
+for i in spam.values():                    #value() method
     print(i)
 
+
 for j in spam.keys():
-    print(j)        #keys() metod
+    print(j)                               #keys() metod
 
 
 for k,m in spam.items():
-    print('key:'+k +' '+ 'value:'+str(m))       #using both methods together
+    print('key:'+k +' '+ 'value:'+m)       #using both methods together
+
+
+#setdefault method
+#if the key is not present in the dictionary then it sets th enew value otherwise does nothing
+
+google={'name':'pooka','age':'5'}
+if 'color' not in google:
+    google['color']='black'     #first way to do
+print(google)
+
+#using setdefault method
+google.setdefault('origin','India')
+print(google)

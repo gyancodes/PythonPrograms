53b5d3d16a43e293f4c12a1a5b2ad78b5ccd9339
ABHAY SHEKHAR YADAV <your-email-address>
2019-05-29 10:58:23 +0530

    using different methods on list

---
+++ methodsOnList.py
@@ -0,0 +1,19 @@
+#methods
+#index method
+
+name=['abhay','shekhar','yadav','tanuj','chraborty']
+print(name.index('shekhar'))    #it prints the index of desired list item
+
+#append method
+name.append('leon') #adds to the list item
+print(name)
+
+#insert method
+
+name.insert(6,'patrick')    #inserts new list item in the desired index
+print(name)
+
+#remove method
+
+name.remove('patrick')
+print(name)

9d56a7f749672840c813a930e16c9ac5d88cab4b
ABHAY SHEKHAR YADAV <your-email-address>
2019-05-29 11:58:41 +0530

    list and tuple type conversion

---
+++ tuplesAndListConversion.py
@@ -0,0 +1,13 @@
+#tuples are similar to list but it is written in small bracket and its values cannot be modified
+
+name=('Abhay','Shekhar','Yadav')   #tuple format
+
+#using list function to convert tuple into list
+
+name=list(('abhay','shekhar'))
+name.append('yadav')
+print(name)    #now slicing can be done after making tuple as a list
+
+# using tuple function to convert list into tuple
+name1=tuple(('abhay','shekhar'))
+#name1.append('yadav')    now it cannot be appended or removed

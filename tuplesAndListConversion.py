5a1552ab0293de340737d8db472e1bfc6572f082
ABHAY SHEKHAR YADAV <your-email-address>
2019-05-29 12:00:36 +0530

    list and tuple type conversion

--- tuplesAndListConversion.py
+++ tuplesAndListConversion.py
@@ -5,6 +5,8 @@ name=('Abhay','Shekhar','Yadav')   #tuple format
 #using list function to convert tuple into list
 
 name=list(('abhay','shekhar'))
+print(name)
+print('after appending')
 name.append('yadav')
 print(name)    #now slicing can be done after making tuple as a list
 

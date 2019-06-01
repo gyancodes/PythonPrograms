a5a755e5fba252d098a1fa4e3c7f956802685e2f
ABHAY SHEKHAR YADAV <your-email-address>
2019-06-02 01:38:08 +0530

    Raw string,multiline string with tripple qoute and multiline comment

---
+++ rawString.py
@@ -0,0 +1,14 @@
+print(r'That is carol\n ')      #prints the string as it is
+
+#multiline string with tripple qoutes
+print(''' Dear Alice,
+Eve's cat has been arrested for catnapping,cat burglary,and extortion.
+Sincerely,
+Bob''')
+
+#multiline comment
+
+"""This is the test python program
+Written by Abhay Shekhar Yadav"""
+"""This program is designed for python 3 not python 2"""
+print('Hello')

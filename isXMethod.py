55d067dc14758b17c3e248643e6fef8016a40822
ABHAY SHEKHAR YADAV <your-email-address>
2019-06-03 16:47:34 +0530

    startswith, endswith methods on strings

---
+++ isXMethod.py
@@ -0,0 +1,10 @@
+alphabet='a'
+print(alphabet.isalpha()) #true if it is alphabet
+alphanumeric='abhay55'
+print(alphanumeric.isalnum()) #true if it is alphanumeric
+
+decimal='123'   #true if it is decimal only
+print(decimal.isdecimal())
+
+title='Abhay Shekhar Yadav'  #true if starting letter is capital
+print(title.istitle())
---
+++ startsWithEndsWith.py
@@ -0,0 +1,6 @@
+start='Hello World'
+print(start.startswith('Hello'))
+
+print(start.endswith('World!'))
+
+print(start.endswith('World'))

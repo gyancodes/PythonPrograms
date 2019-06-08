9163a025331b891c3b3d4f57bee21226acbc9c61
ABHAY SHEKHAR YADAV <your-email-address>
2019-06-08 18:51:10 +0530

    using pipe(|) character to find any one of several options

--- isPhoneNumberRE.py
+++ isPhoneNumberRE.py
@@ -8,6 +8,10 @@ phnregex=re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')  #first parenthesis is for fi
 mob=phnregex.search('My number is 415-654-9874')
 print('Area :'+mob.group(1)+'\t Main Phone number :'+mob.group(2))
 
-'''since mob.group returns tuple we can use as'''
+'''since mob.groups returns tuple we can use as'''
 a,b=mob.groups()
 print(a,b)
+
+batregex=re.compile(r'Bat(man|mobile|copter)')  #re for batman,batmobile,batcopter
+mobj=batregex.search('Batmobile lost a wheel')
+print(mobj.group())

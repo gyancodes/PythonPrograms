5634ac6914a6a098e06d53845e197dad603f171d
ABHAY SHEKHAR YADAV <your-email-address>
2019-06-08 19:43:28 +0530

    '''optional matching wit question mark'''

--- isPhoneNumberRE.py
+++ isPhoneNumberRE.py
@@ -12,6 +12,13 @@ print('Area :'+mob.group(1)+'\t Main Phone number :'+mob.group(2))
 a,b=mob.groups()
 print(a,b)
 
-batregex=re.compile(r'Bat(man|mobile|copter)')  #re for batman,batmobile,batcopter
+batregex=re.compile(r'Bat(man|mobile|copter)')  #re for batman,batmobile,batcopter(several alternative patterns)
 mobj=batregex.search('Batmobile lost a wheel')
 print(mobj.group())
+
+'''optional matching wit question mark'''
+optional=re.compile(r'Bat(wo)?man')
+vs=optional.search('The adventures of Batman')
+print(vs.group())
+vd=optional.search('The adventures of Batwoman')
+print(vd.group())

cf5962d2fbd4cf36aa0a8a9bd776a9209cf8d1c9
ABHAY SHEKHAR YADAV <your-email-address>
2019-06-09 13:24:45 +0530

    --Case insensitive matching--

--- characterClasses.py
+++ characterClasses.py
@@ -35,3 +35,14 @@ print(beginswithandendswith.findall('13148941168443'))
 print('---using wildcard character (.)---')
 wildcard=re.compile(r'.at')                             #matches any character except new line
 print(wildcard.findall('The cat in the hat sat on the flat mat'))
+
+print('---matching everything with dot star---')
+nameregex=re.compile(r'Firstname:(.*) Lastname:(.*)')
+bb=nameregex.search('Firstname:Abhay Lastname:Shekhar')
+print(bb.group(1))
+print(bb.group(2))
+
+print('---Case insensitive matching---')
+reg=re.compile(r'robocop',re.I)         #passing re.I as argument to re.compile to ignore the case of matched string
+pr=reg.search('RoboCop protects the innoncent')
+print(pr.group())

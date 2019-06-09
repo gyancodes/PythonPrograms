ab9da5fb1e077bf07fbc8ff420a91a8406cc4f02
ABHAY SHEKHAR YADAV <your-email-address>
2019-06-09 10:45:56 +0530

    using negative character set

--- characterClasses.py
+++ characterClasses.py
@@ -10,3 +10,9 @@ print(vowelregex.findall('I love you so much'))
 print('---Range of letters using hyphen in the character set---')
 regx=re.compile(r'[a-zA-Z0-9]')         #it will match lowercase letters,uppercase letters and numbers
 print(regx.findall('I love you so much saalu'))
+
+print('---Negative character set---')               #it excludes that character set
+negreg=re.compile(r'[^aeiouAEIOU]')                 #here space is also printed as character
+print(negreg.findall('I love you so much saalu'))
+negregex=re.compile(r'[^aeiouAEIOU\s]')             #here space is not taken
+print(negregex.findall('I love you so much saalu'))
